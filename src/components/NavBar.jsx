d84011c91ba3feebc6cd1d0efac1ece7bfc91a49
juanpbassano <jpbassano@gmail.com>
2024-05-15 19:58:06 -0300

    PreEntrega1Bassano

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,17 @@ import CartWidget from './CartWidget';
 const NavBar = () => {
     return (
         <header>
-            <nav >
+            <nav>
                 <div className='navContainer'>
                     <h1 className='titulo'>Ecomerce con react</h1>
                     <div>
                         <input className='search' id="search" type="search" required placeholder="Buscador" />
                     </div>
                     <ul className='contButtons'>
-                        <a href="#" className='navButtons' >Home</a>
-                        <a href="#" className='navButtons'>Shop</a>
+                        <a href="#" target='_blank' className='navButtons' >Home</a>
+                        <a href="#" target='_blank' className='navButtons'>Shop</a>
                         <CartWidget />
-                        <a href="#" className='navButtons'>Login/Signup</a>
+                        <a href="#" target='_blank' className='navButtons'>Login/Signup</a>
                     </ul>
                 </div>
             </nav>
